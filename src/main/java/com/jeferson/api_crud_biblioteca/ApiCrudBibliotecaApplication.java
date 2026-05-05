2ce2f8a58c518c23935152733e98a96f61b1943a
Jeferson Augusto Schein <schein.jefer@gmail.com>
2026-05-05 20:23:04 -0300

    fet: initial commit

---
+++ src/main/java/com/jeferson/api_crud_biblioteca/ApiCrudBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.jeferson.api_crud_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeferson/api_crud_biblioteca/ApiCrudBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeferson.api_crud_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
